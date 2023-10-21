0651236ceb86c0b7cc3299b823f5e96c2cf1ca62
Chatavut Viriyasuthee <p.virie@gmail.com>
2023-10-21 13:14:06 +0700

    added full eq

--- web/scripts/equation.js
+++ web/scripts/equation.js
@@ -7,11 +7,25 @@ class Equation {
 
         const container = this.group.foreignObject(width, height).move(x, y).addClass("equation-container");
 
-        const content = document.createElement("div");
-        content.classList.add("math");
-        katex.render("c = \\pm\\sqrt{a^2 + b^2}", content, {
+        const content_0 = document.createElement("div");
+        content_0.classList.add("math");
+        katex.render("\\frac{d L_M}{d t} = f(t) - \\lambda L_M ", content_0, {
             throwOnError: false,
         });
-        container.add(SVG(content));
+        container.add(SVG(content_0));
+
+        const content_1 = document.createElement("div");
+        content_1.classList.add("math");
+        katex.render("L_M = \\int_0^t e^{-\\lambda(t - t')}  f(t') \\, d t' ", content_1, {
+            throwOnError: false,
+        });
+        container.add(SVG(content_1));
+
+        const content_2 = document.createElement("div");
+        content_2.classList.add("math");
+        katex.render("f(t) = k p^+ a_T a_M", content_2, {
+            throwOnError: false,
+        });
+        container.add(SVG(content_2));
     }
 }
