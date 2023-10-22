65ca19461cb339eaa78e795498b60e8678b44d71
Chatavut Viriyasuthee <p.virie@gmail.com>
2023-10-22 23:09:47 +0700

    adjust equation

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -35,16 +35,17 @@
         }
 
         renderMathInElement(document.body, {
-            // customised options
-            // • auto-render specific keys, e.g.:
             delimiters: [
                 { left: "$$", right: "$$", display: true },
                 { left: "$", right: "$", display: false },
                 { left: "\\(", right: "\\)", display: false },
                 { left: "\\[", right: "\\]", display: true },
             ],
-            // • rendering keys, e.g.:
             throwOnError: true,
         });
+
+        for (const dom of document.querySelectorAll(".katex span")) {
+            dom.classList.add("tao");
+        }
     });
 })();
